6923019056d147ade668d715118ad6fa45922a67
ShabunyaPolina <73870341+ShabunyaPolina@users.noreply.github.com>
2022-02-22 10:18:36 +0300

    Implemented DOS protection

---
+++ src/main/java/com/es/phoneshop/securiry/impl/DefaultDosProtectionService.java
@@ -0,0 +1,49 @@
+package com.es.phoneshop.securiry.impl;
+
+import com.es.phoneshop.securiry.DosProtectionService;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class DefaultDosProtectionService implements DosProtectionService {
+    private static final long THRESHOLD = 20;
+
+    private final Map<String, Long> countMap = new ConcurrentHashMap<>();
+
+    private DefaultDosProtectionService() {
+        Thread mapCleaner = new Thread(new Thread(() -> {
+            while(true){
+                try {
+                    countMap.clear();
+                    Thread.sleep(20000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e.getCause());
+                }
+            }
+        }));
+        mapCleaner.start();
+    }
+
+    private static final class DosProtectionServiceHolder {
+        private static final DosProtectionService INSTANCE = new DefaultDosProtectionService();
+    }
+
+    public static DosProtectionService getInstance() {
+        return DosProtectionServiceHolder.INSTANCE;
+    }
+
+    @Override
+    public boolean isAllowed(String ip) {
+        Long count = countMap.get(ip);
+        if (count == null) {
+            count = 1L;
+        } else {
+            if(count > THRESHOLD) {
+                return false;
+            }
+            ++count;
+        }
+        countMap.put(ip, count);
+        return true;
+    }
+}
